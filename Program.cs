be51f59b4e778c6a0b2c47b9d791d7e8c0228fff
Ulisses Moraes <ulissesmoraes74@gmail.com>
2023-11-10 20:19:10 -0400

    Pilhas - LIFO - Coleções STACK

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Stack<int> pilha = new Stack<int>();
+
+pilha.Push(02);
+pilha.Push(04);
+pilha.Push(06);
+pilha.Push(08);
+pilha.Push(10);
+pilha.Push(12);
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);
+}
+
+pilha.Pop();
+
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop}");
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);
+}
